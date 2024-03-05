f2a4343fb9c64efdde5d656f59a00d33b260f538
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2024-03-04 22:08:29 -0300

    first commit

---
+++ BuberDinner.Application/Services/Authentication/AuthenticationResult.cs
@@ -0,0 +1,9 @@
+namespace BuberDinner.Services.Authentication;
+
+public record AuthenticationResult (
+    Guid Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Token
+);
---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,26 @@
+using BuberDinner.Services.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    AuthenticationResult IAuthenticationService.Register(string firstName, string lastName, string email, string password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(), 
+            firstName, 
+            lastName, 
+            email, 
+            "token");
+    }
+
+    AuthenticationResult IAuthenticationService.Login(string email, string password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(), 
+            "firstName", 
+            "lastName",
+            email, 
+            "token");
+    }
+}
